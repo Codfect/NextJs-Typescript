afb8a6e063c024709b368716c76e6ffe5a8d5f34
Oliver <oliverjgodoy@gmail.com>
2021-02-26 13:39:58 -0300

    countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
 import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from "../components/Profile"
+import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 
-import styles from '../styles/pages/Home.module.css'
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div>
@@ -20,5 +29,5 @@ export default function Home() {
         </div>
       </section>
     </div>
-  )
+  );
 }
