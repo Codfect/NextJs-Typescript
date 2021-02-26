9219db838b75dc5289e2542e4ace5e95aed2569f
Oliver <oliverjgodoy@gmail.com>
2021-02-26 13:01:57 -0300

    challenges completed

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,15 @@
+import styles from '../styles/components/Profile.module.css'
+
 export function Profile() {
     return(
-        <div>
+        <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/54602909?s=400&u=869175d1dcaa9abf9f6952279f7b3809cbcd26f8&v=4" alt="Profile avatar" />
             <div>
                 <strong>Oliver </strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level up image" />
+                    Level 1
+                </p>
             </div>
         </div>
     );
