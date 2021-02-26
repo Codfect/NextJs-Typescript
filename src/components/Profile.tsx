66d1059b4b02b7b208ce67b98d39d535d154c1a9
Oliver <oliverjgodoy@gmail.com>
2021-02-26 12:25:12 -0300

    profile set

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return(
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/54602909?s=400&u=869175d1dcaa9abf9f6952279f7b3809cbcd26f8&v=4" alt="Profile avatar" />
+            <div>
+                <strong>Oliver </strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
