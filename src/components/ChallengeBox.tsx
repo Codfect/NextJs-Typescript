0910d8ba37d26e66a67ddda6303d70f17ca4cd15
Oliver <oliverjgodoy@gmail.com>
2021-02-27 18:26:23 -0300

    active, and finish layout

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,35 @@
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
+    const activeChallenge = true;
+
+
     return(
         <div className={styles.challengeBoxContainer}>
-            <div className={styles.challengeNotActive}>
-                <strong>Finalize um ciclo para receber um desafio</strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up" />
-                    Avance de level completando desafios.
-                </p>
-            </div>
+            { activeChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400 xp</header>
+
+                    <main>
+                        <img src="icons/body.svg" />
+                        <strong>Novo desafio</strong>
+                        <p>Teste e veja a descrição do desafio</p>
+                    </main>
+
+                    <footer>
+                        <button type="button" className={styles.challengeFail}>Falhei </button>
+                        <button type="button" className={styles.challengeSuccess}>Completei</button>
+                    </footer>
+                </div>
+            ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        <img src="icons/level-up.svg" alt="Level Up" />
+                        Avance de level completando desafios.
+                    </p>
+                </div>
+            ) }
         </div>
     );
 }
