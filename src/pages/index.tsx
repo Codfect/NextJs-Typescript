6ad7cf6fc597368c18142c9f7bcdabb6877a057d
Oliver <oliverjgodoy@gmail.com>
2021-03-04 13:50:29 -0300

    storage with cookies works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next';
+
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
@@ -8,29 +10,64 @@ import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 import { CountDownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from '../contexts/ChallengeContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}
+    >
+  
+      <div className={styles.container}>
+        <Head>
+          <title>Home Page</title>
+        </Head>
 
-      <Head>
-        <title>Home Page</title>
-      </Head>
+        <ExperienceBar />
+        <CountDownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
 
-      <ExperienceBar />
-      <CountDownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountDownProvider>
-    </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountDownProvider>
+      </div>
+    </ChallengesProvider>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const user = {
+    level: 1,
+    currentExperience: 50,
+    challengesCompleted: 2,
+  }
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  console.log(user);
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
+
+ 
